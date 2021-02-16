cc84a267f1537abaa546f607f7b494ef9015097b
Johnny Kang <junyoung@my.yorku.ca>
2021-02-15 19:17:46 -0500

    Updated Map Click and Unclick

--- src/mapComponent/MyMap.jsx
+++ src/mapComponent/MyMap.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MapContainer, GeoJSON } from "react-leaflet";
+import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
 import countries from '../data/countries.json';
 import "leaflet/dist/leaflet.css";
 import "./MyMap.css";
@@ -9,10 +9,6 @@ class MyMap extends Component {
 
     color = ["green", "blue", "orange", "grey", "red", "purple", "yellow", "brown", "pink"]
 
-    componentDidMount(){
-        
-    }
-
     countryStyle = {
         fillColor: "green", //country color
         fillOpacity: 1, // number can go from 0-1
@@ -21,33 +17,40 @@ class MyMap extends Component {
         dashArray: 1, // if you want to make border into dashed line
     }
 
-    onCountryClick = (event) => {
-
-    };
-
     changeCountryColor = (event)=> {
-        event.target.setStyle(
-            {
-                color: "white", //border color
-                fillColor: this.state.color,
-                fillOpacity: 0.5,
-            }
-        )
+        if (event.target.options.fillColor === "green") { //Change Color
+            event.target.setStyle(
+                { 
+                    color: "white", //border color
+                    fillColor: this.state.color,
+                    fillOpacity: 0.5,
+                }
+            )
+        }
+        else if (event.target.options.fillColor !== "green") { //Change Color
+            event.target.setStyle(
+                { 
+                    color: "black", //border color
+                    fillColor: "green",
+                    fillOpacity: 1,
+                }
+            )
+        }
     }
 
     onEachCountry = (country, layer) => {
         const countryName = country.properties.ADMIN;
-        layer.bindPopup(countryName + " Population: 1000");
-
-        layer.options.fillOpacity = Math.random() + 0.2; // number can go from 0-9
+        layer.bindPopup(countryName + " ");
+        // layer.options.fillOpacity = 0.2; // number can go from 0-9
         // If you want to make countries have different colors use below-------------------------
         // const colorIndex = Math.floor(Math.random() * this.color.length);
         // layer.options.fillColor = this.color[colorIndex]; // number can go from 0-9
 
         layer.on({ //Clickable function
             click: this.changeCountryColor,
-            // mouseover: this.changeCountryColor,
         })
+        layer.on('mouseover', function() { layer.openPopup(); }); //Show country names
+        layer.on('mouseout', function() { layer.closePopup(); });
     }
     
     colorChange = (event) => {
@@ -58,8 +61,8 @@ class MyMap extends Component {
         return ( 
         <div>
             <h1 style ={{textAlign: "center"}}> MyMap</h1>
-            <MapContainer style = {{height: "80vh" }} zoom = {2} center = {[20, 100]}>
-                <GeoJSON style = {this.countryStyle} data = {countries.features} onEachFeature={this.onEachCountry}/>
+            <MapContainer style = {{height: "80vh" }} zoom = {1} minZoom = {1} center = {[50, 13]}>{/* Displays the zoom button*/}
+                <GeoJSON style = {this.countryStyle} data = {countries.features} onEachFeature={this.onEachCountry}/>{/* Displays the map*/}
             </MapContainer>
             <input type = "color" value = {this.state.color} onChange={this.colorChange}></input>
         </div> 
